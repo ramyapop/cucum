6af5fb07de38f2b8fc5b1ff2ffbe065cb8f383ed
ramyapop <ramyamgcworld@gmail.com>
2023-02-24 18:51:40 +0530

    commit message

---
+++ Runner.java
@@ -0,0 +1,41 @@
+package com.runner;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import com.Baseclass.Base_Class;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features=".\\Feature file",glue="com.step",plugin = {"json:adactin_report/adac.json","html:adactin_report/adachtml.html"} )//feature file path + step definition mapping
+
+//plugin will generate report u can use either one json or html
+public class Runner extends Base_Class {//RUNNER CLS
+	
+	
+	
+	public static WebDriver web;
+	
+	@BeforeClass
+	public static void setup() {
+		
+		browser("chromeDriver");
+		max();
+		
+}
+	@AfterClass
+	public static void teardown() {
+
+		quit();
+		
+		
+		
+	
+	}
+
+}
